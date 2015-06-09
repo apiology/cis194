7aa91342f52382538645fce9febad7626aa63a05
Vince Broz <vince@broz.cc>
2015-06-09 13:01:43 -0400

    Decompose a bit

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -3,16 +3,23 @@ module LogAnalysis where
 
 import Log
 
--- read
--- lines, words, unwords, take, drop, and (.).)
+parseTimestampAndRest :: [String] -> Maybe (TimeStamp, String)
+parseTimestampAndRest (timestamp:messages) = Just ((read timestamp), (unwords messages))
+parseTimestampAndRest _ = Nothing
 
--- XXX: Really want to combine code that parses the rest here:
+parseMessageType :: [String] -> Maybe (MessageType, [String])
+parseMessageType ("E":severity:rest) = Just ((Error (read severity)), rest)
+parseMessageType ("I":rest) = Just (Info, rest)
+parseMessageType ("W":rest) = Just (Warning, rest)
+parseMessageType _ = Nothing
 
 parseMessageFromList :: [String] -> LogMessage
-parseMessageFromList ("E":severity:timestamp:messages) = LogMessage (Error (read severity)) (read timestamp) (unwords messages)
-parseMessageFromList ("W":timestamp:messages) = LogMessage Warning (read timestamp) (unwords messages)
-parseMessageFromList ("I":timestamp:messages) = LogMessage Info (read timestamp) (unwords messages)
-parseMessageFromList messages = Unknown (unwords messages)
-
+parseMessageFromList l = 
+    case parseMessageType l of
+      Just (messagetype, rest) -> case parseTimestampAndRest rest of
+                                  Just (timestamp, message) -> LogMessage messagetype timestamp message
+                                  Nothing -> Unknown (unwords l)
+      Nothing                  -> Unknown (unwords l)
+    
 parseMessage :: String -> LogMessage
 parseMessage s = parseMessageFromList (words s)
