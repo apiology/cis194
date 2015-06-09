42392f2f474c6ea1dd31f01731fe3c32cf061a1d
Vince Broz <vince@broz.cc>
2015-06-09 17:15:11 -0400

    Clear out tasks

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -7,18 +7,14 @@ skipsN xs n = case drop (n - 1) xs of
               [] -> []
               (y:ys) -> y : (skipsN ys n)
 
-
 skips :: [a] -> [[a]]
 skips list = map (skipsN list) [1..length list]
-
--- XXX simplify
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima (x1:x2:x3:xs)
   | x2 > x1 && x2 > x3 = x2 : (localMaxima (x2:x3:xs))
   | otherwise = localMaxima (x2:x3:xs)
 localMaxima _ = []
--- XXX filter with >
 
 counts :: [Integer] -> [Int]
 counts xs = map (\n -> length (filter ((==) n) xs)) [0..9]
