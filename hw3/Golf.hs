5e51c92d2197294b624d9e5d575c78fcc81f04f3
Vince Broz <vince@broz.cc>
2015-06-09 15:28:04 -0400

    First cut at localMaxima

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -11,3 +11,12 @@ skipsN xs n = case drop (n - 1) xs of
 
 skips :: [a] -> [[a]]
 skips list = map (skipsN list) [1..length list]
+
+-- XXX simplify
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x1:x2:x3:xs)
+  | x2 > x1 && x2 > x3 = x2 : (localMaxima (x2:x3:xs))
+  | otherwise = localMaxima (x2:x3:xs)
+localMaxima _ = []
+-- XXX filter with >
