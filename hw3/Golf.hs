3d1b7801f25062ceb2d31a6f4f145ccd6822ad4c
Vince Broz <vince@broz.cc>
2015-06-09 15:57:47 -0400

    First cut at histogram

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -20,3 +20,22 @@ localMaxima (x1:x2:x3:xs)
   | otherwise = localMaxima (x2:x3:xs)
 localMaxima _ = []
 -- XXX filter with >
+
+countOf :: [Integer] -> Integer -> Int
+countOf xs n = length (filter ((==) n) xs)
+
+counts :: [Integer] -> [Int]
+counts xs = map (countOf xs) [0..9]
+
+renderStarIfGTE :: Int -> Int -> Char
+renderStarIfGTE value a = if a >= value then '*' else ' '
+
+renderRow :: [Int] -> Int -> String
+renderRow counts v = (map (renderStarIfGTE v) counts) ++ "\n"
+
+renderChart :: [Int] -> String
+renderChart counts = foldl1 (++) (map (renderRow counts) [topValue,topValue-1..0])
+   where topValue = maximum counts
+
+histogram :: [Integer] -> String
+histogram numbers = (renderChart (counts numbers)) ++ "==========\n0123456789\n"
