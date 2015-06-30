3365a826b74fd1167b88ed4e5cf3ba3c96684ccc
Vince Broz <vince@broz.cc>
2015-06-29 21:51:56 -0400

    Finish hw

--- hw12/Risk.hs
+++ hw12/Risk.hs
@@ -2,6 +2,7 @@
 
 module Risk where
 
+import Control.Monad.Loops
 import Control.Monad.Random
 import Control.Monad
 import Data.List
@@ -30,25 +31,9 @@ type Army = Int
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
   deriving (Show)
 
-numAttackerDiceRolls :: Battlefield -> Army
-numAttackerDiceRolls battlefield = min 3 $ attackers battlefield - 1
-numDefenderDiceRolls :: Battlefield -> Army
-numDefenderDiceRolls battlefield = min 2 $ defenders battlefield
-
 sortDesc :: Ord a => [a] -> [a]
 sortDesc = sortBy (flip compare)
 
-attackerRolls :: Battlefield -> Rand StdGen [DieValue]
-attackerRolls battlefield = liftM sortDesc $ replicateM (numAttackerDiceRolls battlefield) die
-
-defenderRolls :: Battlefield -> Rand StdGen [DieValue]
-defenderRolls battlefield = liftM sortDesc $ replicateM (numDefenderDiceRolls battlefield) die
-
-rollResults :: Battlefield -> Rand StdGen [Bool]
-rollResults battlefield = liftM2 (zipWith (>)) aRolls dRolls
-  where aRolls = attackerRolls battlefield
-        dRolls = defenderRolls battlefield
-                   
 testBf :: Battlefield
 testBf = Battlefield 10 2
 
@@ -57,12 +42,42 @@ testBf = Battlefield 10 2
 -- casualties. You may assume that each player will attack or defend
 -- with the maximum number of units they are allowed."
 battle :: Battlefield -> Rand StdGen Battlefield
-battle battlefield = liftM2 Battlefield newAttackerStrength newDefenderStrength
-    where newAttackerStrength = attackerLosses >>= (\a -> return (attackers battlefield - a))
-          newDefenderStrength = defenderLosses >>= (\a -> return (defenders battlefield - a))
-          attackerLosses = liftM (length . filter (==True)) roll
-          defenderLosses = liftM (length . filter (==False)) roll
-          roll = rollResults battlefield
+battle battlefield = do
+  let numDRolls = min 2 $ defenders battlefield
+  rawDRolls <- replicateM numDRolls die
+  let dRolls = sortDesc rawDRolls
+      numARolls = min 3 $ attackers battlefield - 1
+  rawARolls <- replicateM numARolls die
+  let aRolls = sortDesc rawARolls
+  let roll = zipWith (>) aRolls dRolls
+      attackerLosses = length $ filter (==True) roll
+      newAttackerStrength = attackers battlefield - attackerLosses
+      defenderLosses = length roll - attackerLosses
+      newDefenderStrength = defenders battlefield - defenderLosses
+  return $ Battlefield newAttackerStrength newDefenderStrength
 
--- XXX: Go back and make the above prettier
+canBattle :: Battlefield -> Bool
+canBattle battlefield = (attackers battlefield >= 2) && (defenders battlefield > 0)
 
+invade :: Battlefield -> Rand StdGen Battlefield
+invade battlefield =
+  if canBattle battlefield
+  then
+     do
+       newBattlefield <- battle battlefield
+       invade newBattlefield
+   else
+     return battlefield
+
+attackerWon :: Battlefield -> Bool
+attackerWon battlefield = defenders battlefield == 0
+
+characterize :: [Battlefield] -> Double
+characterize endgames = numAttackerWins / numEndgames
+  where numEndgames = fromIntegral $ length endgames
+        numAttackerWins = fromIntegral $ length (filter attackerWon endgames)
+
+successProb :: Battlefield -> Rand StdGen Double
+successProb battlefield = do
+  invasions <- replicateM 1000 $ invade battlefield
+  return $ characterize invasions
